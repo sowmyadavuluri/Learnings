3baa61549bb005d6fa111dfadb1110d31e09b65b
sowmyadavuluri <69600114+sowmyadavuluri@users.noreply.github.com>
2020-08-25 14:54:11 +0530

    Add files via upload

---
+++ insert_data.mysql.sql
@@ -0,0 +1,7 @@
+SELECT * FROM workdb.cable_customers;
+INSERT INTO workdb.cable_customers 
+VALUE 
+     (2,'Sowmya' , 0.15, '2020-01-01','2021-01-01'),
+     (3,'Kranthi', 0.15, '2019-01-01','2022-01-01'),
+     (4,'Anitha', 0.10, '2019-02-02','2022-02-02')
+     ;
